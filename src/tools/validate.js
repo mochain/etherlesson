a70500a89b107e3938bd5b0b5321446f15b0e799
ysqi <devysq@gmail.com>
2018-04-11 07:01:06 +0800

    自定义验证辅助

---
+++ src/tools/validate.js
@@ -0,0 +1,24 @@
+export default {  
+   /**
+    * 验证给定 url 是否是合法格式
+    * @param {string} url 
+    *   需要校验的 URL
+    * @returns
+    *   是否合法
+    */
+   isURL(url) {
+    var strRegex =
+      "^((https|http|ftp|rtsp|mms)?://)" +
+      "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" + // ftp的user@
+      "(([0-9]{1,3}.){3}[0-9]{1,3}" + // IP形式的URL- 199.194.52.184
+      "|" + // 允许IP和DOMAIN（域名）
+      "([0-9a-z_!~*'()-]+.)*" + // 域名- www.
+      "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]." + // 二级域名
+      "[a-z]{2,6})" + // first level domain- .com or .museum
+      "(:[0-9]{1,4})?" + // 端口- :80
+      "((/?)|" + // a slash isn't required if there is no file name
+      "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+    var re = new RegExp(strRegex);
+    return re.test(url);
+  }
+}
